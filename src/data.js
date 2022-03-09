a9154f6fb33f8ef702d71171185de801e69292f3
roxanaggr <roxanaggr@gmail.com>
2022-03-08 22:57:18 -0600

    funcion stats

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+
 //Funcion para filtrar por rol
 export const filterRole = (champions, roles) =>{
    const filterChampions = champions.filter(champion => champion.tags.includes(roles));
@@ -37,5 +38,47 @@ export const filterDifficulty= (champions, difficulty) => {
          }else if (difficulty == 'high'){
             return emptyHig
          }
-};
+}
+
+//Saca las stats y muestra los campeones
+export const filterStats = (champions, stats) => {
+   let topDamage = [];
+   let topHealth = [];
+   let topMana = []; 
+   champions.forEach(champion => {
+      const baseDamage = champion.stats.attackdamage
+      const growthDamage = champion.stats.attackdamageperlevel
+      const averageDamage = parseInt(baseDamage/growthDamage)
+      if (averageDamage > 25 || averageDamage === 40) {
+         topDamage.push(champion)
+      }
+
+      const baseHealth = champion.stats.hpregen
+      const growthHealth = champion.stats.hpregenperlevel   
+      const averageHealth = parseInt(baseHealth/growthHealth)
+      if (averageHealth === 15) {
+         topHealth.push(champion)
+      }
+      
+      const baseMana = champion.stats.mp
+      const growthMana = champion.stats.mpperlevel
+      const averageMana = parseInt(baseMana/growthMana)
+      if (averageMana === 10) {
+         topMana.push(champion)
+      }
+   });
+   if (stats == 'attackdamage'){
+      return topDamage
+   }else if (stats == 'hpregen'){
+      return topHealth
+   }else if (stats == 'mana'){
+      return topMana
+   }
+   
+}
+
+   
+
+
+
 
